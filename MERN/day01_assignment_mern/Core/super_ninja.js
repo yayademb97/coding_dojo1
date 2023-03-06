8912e79c9832aec5ea3ace56719ddcabb604bb1c
Yaya Dembele <yayadembele09@gmail.com>
2023-03-06 23:55:14 +0100

    super_ninja.js

--- MERN/day01_assignment_mern/Core/super_ninja.js
+++ MERN/day01_assignment_mern/Core/super_ninja.js
@@ -1,6 +1,31 @@
+class Ninja {
+    constructor(name) {
+        this.name = name;
+        this.health = 100;
+        this.speed = 3;
+        this.strength = 3;
+    }
+
+    sayName() {
+        console.log(`My ninja name is ${this.name}!`);
+    }
+
+    showStats() {
+        console.log(`Name: ${this.name}, Health: ${this.health}, Speed: ${this.speed}, Strength: ${this.strength}`);
+    }
+
+    drinkSake() {
+        this.health += 10;
+        console.log(`You drank sake and gained 10 health!`);
+    }
+}
+
 class Sensei extends Ninja {
     constructor(name) {
-        super(name, 200, 10, 10);
+        super(name);
+        this.health = 200;
+        this.speed = 10;
+        this.strength = 10;
         this.wisdom = 10;
     }
 
@@ -9,3 +34,7 @@ class Sensei extends Ninja {
         console.log("What one programmer can do in one month, two programmers can do in two months.");
     }
 }
+
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+superSensei.showStats();
