42b9dcef66136a68525633a08f55029588ebddf8
Yaya Dembele <yayadembele09@gmail.com>
2023-03-09 00:36:22 +0100

    first-react-with-jsx

---
+++ MERN/react/Day01/Core/first-react-with-jsx/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import MyNewComponent from './components/MyNewComponent;'
+
+function App() {
+  return (
+    <div className="App">
+      <MyNewComponent  />
+    </div>
+  );
+}
+
+export default App;
---
+++ MERN/react/Day01/Core/first-react-with-jsx/src/components/MyNewComponent.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+class MyNewComponent extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello Dojo</h1>
+                <h3>Things I need to do</h3>
+                <ul>
+                    <li>Learn React</li>
+                    <li>Climb Mt Everest</li>
+                    <li>Run a marathon</li>
+                    <li>Feed the dogs</li>
+
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default MyNewComponent;
