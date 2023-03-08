68e70393bd2ec6aec53c7aaca7db180c6a17bbab
Yaya Dembele <yayadembele09@gmail.com>
2023-03-08 19:55:53 +0100

    first-react

---
+++ MERN/react/first-react/first-react/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import SomeClassComponent from './components/SomeClassComponent';
+function App() {
+  return (
+    <div className="App">
+      <h1>First React page rendering...</h1>
+      <SomeClassComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ MERN/react/first-react/first-react/src/components/SomeClassComponent.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class SomeClassComponent extends Component {
+    render() {
+        return (
+            <div>SomeClassComponent
+                <p>Now It's not easy life with MERN Stack</p></div>
+        )
+    }
+}
+export default SomeClassComponent;
